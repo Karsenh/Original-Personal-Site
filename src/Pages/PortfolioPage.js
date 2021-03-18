f31160dff43f01545cdd3cba2252e097312551c8
Karsenh <KarsenHansen@gmail.com>
2021-03-17 19:14:32 -0700

    Portfolio page done

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -4,16 +4,33 @@ import MenuItem from '../components/MenuItem';
 import Title from '../components/Title';
 import portfolios from '../components/allPortfolios';
 
+// Create an array with unique categories
+const allCategories = [
+  'All',
+  ...new Set(portfolios.map(item => item.category)),
+];
+
 function PortfolioPage() {
-  const [categories, setCategories] = useState(null);
+  const [categories, setCategories] = useState(allCategories);
   const [menuItems, setMenuItems] = useState(portfolios);
+
+  const filter = category => {
+    if (category === 'All') {
+      setMenuItems(portfolios);
+      return;
+    }
+    const filteredData = portfolios.filter(item => {
+      return item.category === category;
+    });
+    setMenuItems(filteredData);
+  };
   return (
     <div className='PortfolioPage'>
       <div className='title'>
         <Title title={'Portfolio'} span={'Portfolio'}></Title>
       </div>
       <div className='portfolio-data'>
-        <Categories />
+        <Categories filter={filter} categories={categories} />
         <MenuItem menuItem={menuItems} />
       </div>
     </div>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-function Categories() {
-  return <div></div>;
+function Categories({ filter, categories }) {
+  return (
+    <div className='buttons'>
+      {categories.map((cat, i) => {
+        return (
+          <button
+            type='button'
+            key={i}
+            className='btn-port'
+            onClick={() => filter(cat)}
+          >
+            {cat}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Categories;
